9850b15e07642a34a4b04509fc8ad173553544e2
Alin Motogna <alin.motogna@tora.com>
2016-11-12 14:16:13 +0800

    Add FlightSearch

---
+++ FlightSearch/FlightData.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections.Generic;
+
+namespace FlightSearch
+{
+    public class Airport
+    {
+        public string kind { get; set; }
+        public string code { get; set; }
+        public string city { get; set; }
+        public string name { get; set; }
+    }
+
+    public class City
+    {
+        public string kind { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Aircraft
+    {
+        public string kind { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Tax
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Carrier
+    {
+        public string kind { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Data
+    {
+        public string kind { get; set; }
+        public List<Airport> airport { get; set; }
+        public List<City> city { get; set; }
+        public List<Aircraft> aircraft { get; set; }
+        public List<Tax> tax { get; set; }
+        public List<Carrier> carrier { get; set; }
+    }
+
+    public class Flight
+    {
+        public string carrier { get; set; }
+        public string number { get; set; }
+    }
+
+    public class Leg
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public string aircraft { get; set; }
+        public string arrivalTime { get; set; }
+        public string departureTime { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public string originTerminal { get; set; }
+        public int duration { get; set; }
+        public int onTimePerformance { get; set; }
+        public int mileage { get; set; }
+        public bool secure { get; set; }
+        public string destinationTerminal { get; set; }
+        public string meal { get; set; }
+        public string operatingDisclosure { get; set; }
+        public int? connectionDuration { get; set; }
+        public bool? changePlane { get; set; }
+    }
+
+    public class Segment
+    {
+        public string kind { get; set; }
+        public int duration { get; set; }
+        public Flight flight { get; set; }
+        public string id { get; set; }
+        public string cabin { get; set; }
+        public string bookingCode { get; set; }
+        public int bookingCodeCount { get; set; }
+        public string marriedSegmentGroup { get; set; }
+        public List<Leg> leg { get; set; }
+        public int connectionDuration { get; set; }
+    }
+
+    public class Slouse
+    {
+        public string kind { get; set; }
+        public int duration { get; set; }
+        public List<Segment> segment { get; set; }
+    }
+
+    public class Fare
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public string carrier { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public string basisCode { get; set; }
+    }
+
+    public class BagDescriptor
+    {
+        public string kind { get; set; }
+        public string commercialName { get; set; }
+        public int count { get; set; }
+        public string subcode { get; set; }
+        public List<string> description { get; set; }
+    }
+
+    public class FreeBaggageOption
+    {
+        public string kind { get; set; }
+        public List<BagDescriptor> bagDescriptor { get; set; }
+        public int pieces { get; set; }
+    }
+
+    public class SegmentPricing
+    {
+        public string kind { get; set; }
+        public string fareId { get; set; }
+        public string segmentId { get; set; }
+        public List<FreeBaggageOption> freeBaggageOption { get; set; }
+    }
+
+    public class Passengers
+    {
+        public string kind { get; set; }
+        public int adultCount { get; set; }
+    }
+
+    public class Tax2
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public string chargeType { get; set; }
+        public string code { get; set; }
+        public string country { get; set; }
+        public string salePrice { get; set; }
+    }
+
+    public class Pricing
+    {
+        public string kind { get; set; }
+        public List<Fare> fare { get; set; }
+        public List<SegmentPricing> segmentPricing { get; set; }
+        public string baseFareTotal { get; set; }
+        public string saleFareTotal { get; set; }
+        public string saleTaxTotal { get; set; }
+        public string saleTotal { get; set; }
+        public Passengers passengers { get; set; }
+        public List<Tax2> tax { get; set; }
+        public string fareCalculation { get; set; }
+        public string latestTicketingTime { get; set; }
+        public string ptc { get; set; }
+    }
+
+    public class TripOption
+    {
+        public string kind { get; set; }
+        public string saleTotal { get; set; }
+        public string id { get; set; }
+        public List<Slouse> slice { get; set; }
+        public List<Pricing> pricing { get; set; }
+    }
+
+    public class Trips
+    {
+        public string kind { get; set; }
+        public string requestId { get; set; }
+        public Data data { get; set; }
+        public List<TripOption> tripOption { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string kind { get; set; }
+        public Trips trips { get; set; }
+    }
+}
---
+++ FlightSearch/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace FlightSearch
+{
+    class Program
+    {
+        // Demo code for QPX Express API
+        // Free requests limited to 50 per day
+        // Billing must be activated at https://console.developers.google.com/ to allow more
+        static void Main(string[] args)
+        {
+            #region parameters
+            // API key needs to be generated at https://console.developers.google.com/
+            // Add a new project, add the QPX Express API to it, and generate a server key
+            // For security, you can specify only requests allowed from certain IPs
+            string API_KEY = "AIzaSyDLo2QRtu4g-k-hPUv3kLRY5c6dDMWOv_0";
+            string URL = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + API_KEY;
+
+            // one or more required
+            int adultCount = 1;
+            int childCount = 0;
+            int infantInLapCount = 0;
+            int infantInSeatCount = 0;
+            int seniorCount = 0;
+
+            // required
+            string origin = "ELP";
+            string destination = "IAH";
+            string date = "2015-02-05";
+            bool refundable = true;
+            int solutions = 50;
+
+            // optional
+            int maxStops = 2;
+            string earliestDepartureTime = null;
+            string latestDepartureTime = null;
+            #endregion
+
+            #region craft JSON
+            string json = @"{""request"": {""passengers"": {";
+            if (adultCount > 0)
+                json += @"""adultCount"": " + adultCount;
+            if (childCount > 0)
+                json += @",""childCount"": " + childCount;
+            if (infantInLapCount > 0)
+                json += @",""infantInLapCount"": " + infantInLapCount;
+            if (infantInSeatCount > 0)
+                json += @",""infantInSeatCount"": " + infantInSeatCount;
+            if (seniorCount > 0)
+                json += @",""seniorCount"": " + seniorCount;
+            json += @"},""slice"": [{";
+            json += @"""origin"": """ + origin + @"""";
+            json += @",""destination"": """ + destination + @"""";
+            json += @",""date"": """ + date + @"""";
+            if (maxStops > 0)
+                json += @",""maxStops"": " + maxStops;
+            if (earliestDepartureTime != null || latestDepartureTime != null)
+                json += @",""permittedDepartureTime"": {";
+            if (earliestDepartureTime != null)
+                json += @"""earliestTime"": """ + earliestDepartureTime + @"""";
+            if (latestDepartureTime != null)
+                json += @",""latestTime"": """ + latestDepartureTime + @"""";
+            if (earliestDepartureTime != null || latestDepartureTime != null)
+                json += "}";
+            json += @"}],";
+            json += @"""refundable"": " + refundable.ToString().ToLower() + ",";
+            if (solutions > 0)
+                json += @"""solutions"": " + solutions;
+            json += "}}";
+            // Console.WriteLine(json);
+            #endregion
+            #region make POST
+            // make a POST to the proper site with the JSON
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(URL);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            var result = "";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    result = streamReader.ReadToEnd();
+
+                }
+            }
+            #endregion
+            #region print results to file and console
+            // System.IO.File.WriteAllText("result.txt", result);
+            //Console.WriteLine(result);
+            System.IO.File.WriteAllText("result.txt", "");
+            RootObject res = new RootObject();
+            res = JsonConvert.DeserializeObject<RootObject>(result);
+            int counter = 1;
+            foreach (TripOption op in res.trips.tripOption)
+            {
+                WriteLine("Trip Option " + counter);
+                WriteLine("Total Price: " + op.saleTotal);
+                int counter2 = 1;
+                foreach (Slouse slice in op.slice)
+                {
+                    foreach (Segment seg in slice.segment)
+                    {
+                        string carrierID = seg.flight.carrier;
+                        string carrier = "";
+                        string number = seg.flight.number;
+                        foreach (Carrier carr in res.trips.data.carrier)
+                        {
+                            if (carr.code.Equals(carrierID))
+                                carrier = carr.name;
+                        }
+                        int seats = seg.bookingCodeCount;
+                        foreach (Leg leg in seg.leg)
+                        {
+                            WriteLine("Flight " + counter2 + ":\t\t" + carrierID + " " + number);
+                            counter2++;
+                            WriteLine("Origin: " + leg.origin + "\t\tTerminal: " + leg.originTerminal);
+                            WriteLine("Departure Time: " + DateTime.Parse(leg.departureTime).ToString("g"));
+                            WriteLine("Destination: " + leg.destination + "\tTerminal: " + leg.destinationTerminal);
+                            WriteLine("Arrival Time: " + DateTime.Parse(leg.arrivalTime).ToString("g"));
+                            WriteLine("Carrier: " + carrier);
+                            WriteLine("Duration: " + leg.duration + " minutes");
+                            WriteLine("Seats Available: " + seats);
+                        }
+                    }
+                }
+                WriteLine("");
+                counter++;
+            }
+            Console.ReadKey();
+            #endregion
+        }
+        public static void WriteLine(string str)
+        {
+            Console.WriteLine(str);
+            System.IO.File.AppendAllText("result.txt", str + "\n");
+        }
+    }
+}
